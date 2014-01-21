5a40131170d53d38a45b100ff37fec05921f0083
MintPaw <jerusanders@gmail.com>
2014-01-21 13:50:16 -0700

    Project start.
    The projects start, just includes a Unity file with a background and a
    blue "player" square.

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+	public static float deadzone = .1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float hInput = Input.GetAxis("Horizontal");
+		float vInput = Input.GetAxis("Vertical");
+	}
+}
